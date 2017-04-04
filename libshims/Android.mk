40a33bd257e139dcba435ca1c4ce4db908a0d57d
theSt33v <steve.wenzl@gmail.com>
2017-04-04 16:25:32 -0400

    Added IMS libshim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -28,3 +28,11 @@ LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
 LOCAL_MODULE := libshim_qcopt
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+#From MSM8916-common
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := MediaBuffer.c
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
